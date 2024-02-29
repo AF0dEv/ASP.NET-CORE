895b1773cc8473668141d3331ba388ce46332683
Afonso <afonsopro.business@gmail.com>
2024-02-29 10:21:02 +0000

    first project commit

---
+++ FIRST_ASP_PROJECT/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FIRST_ASP_PROJECT.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
