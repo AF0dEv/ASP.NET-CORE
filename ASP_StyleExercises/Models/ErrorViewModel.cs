8d87c5028021f306874c4bf05138c84563949f48
Afonso <afonsopro.business@gmail.com>
2024-03-05 09:40:43 +0000

    Style Exercise

---
+++ ASP_StyleExercises/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ASP_StyleExercises.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
