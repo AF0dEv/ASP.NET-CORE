932a0cad73d28c6fbf132dabacd9b9dfa8df0f1a
AF0dEv <afonsopro.business@gmail.com>
2024-03-16 18:28:40 +0000

    exercise 26 done, 3 more to go, styling in progress

--- EquipasMembros/Controllers/C26_QuantosMembrosEquipaMaxiFalcoes.cs
+++ EquipasMembros/Controllers/C26_QuantosMembrosEquipaMaxiFalcoes.cs
@@ -13,10 +13,9 @@ namespace EquipasMembros.Controllers
         }
         public IActionResult Index()
         {
-            var equipa = dbp.Tequipas.Where(e => e.NomeEquipa.Equals("Maximinense")).ToList();
             var contagem = dbp.Tmembros.Join(dbp.Tequipas, m => m.EquipaId, e => e.Id,
                 (m, e) => new { m, e })
-                .Where(m => m.e.NomeEquipa.Equals("Maximinense") && m.e.NomeEquipa.Equals("Falcões do Picoto"))
+                .Where(m => m.e.NomeEquipa.Equals("Maximinense") || m.e.NomeEquipa.Equals("Falcões do Picoto"))
                 .Count(); // onde o nome da equipa é igual a "Maximinense" e "Falcões do Picoto", conta os membros
             ViewBag.CONTAGEM = contagem;
             return View();
