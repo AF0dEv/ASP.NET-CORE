20d2c55f824a7d5df34393f6fd1992709515057a
AF0dEv <afonsopro.business@gmail.com>
2024-03-18 16:56:33 +0000

    new exercises

---
+++ ExCliX/Controllers/CD2.cs
@@ -0,0 +1,27 @@
+﻿using ExCliX.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace ExCliX.Controllers
+{
+    public class CD2 : Controller
+    {
+        ApplicationDbContext dbp;
+        public CD2(ApplicationDbContext context)
+        {
+            dbp = context;
+        }
+        public IActionResult Index(string? txtNome)
+        {
+            if (txtNome != null)
+            {
+                ViewBag.LISTACLIENTES = new SelectList(dbp.Tclientes.Where(c => c.Nome.Contains(txtNome)), "Id", "Nome");
+            }
+            else
+            {
+                ViewBag.LISTACLIENTES = new SelectList(dbp.Tclientes, "Id", "Nome");
+            }
+            return View();
+        }
+    }
+}
---
+++ ExCliX/Controllers/CD3.cs
@@ -0,0 +1,38 @@
+﻿using ExCliX.Data;
+using ExCliX.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExCliX.Controllers
+{
+    public class CD3 : Controller
+    {
+        ApplicationDbContext dbp;
+        public CD3(ApplicationDbContext context)
+        {
+            dbp = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Criar()
+        { 
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Criar(string txtNome, string txtReferencia)
+        {
+            if (txtNome != null && txtReferencia != null)
+            {
+                Cliente c = new Cliente();
+                c.Nome = txtNome;
+                c.Referencia = txtReferencia;
+                dbp.Tclientes.Add(c);
+                dbp.SaveChanges();
+            }
+            return View();
+        }
+    }
+}
