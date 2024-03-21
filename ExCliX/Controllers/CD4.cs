73bc525462563be6627b15c2525f8237072902f6
AF0dEv <afonsopro.business@gmail.com>
2024-03-21 14:14:20 +0000

    more exercises on CliX

---
+++ ExCliX/Controllers/CD4.cs
@@ -0,0 +1,28 @@
+﻿using ExCliX.Data;
+using ExCliX.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExCliX.Controllers
+{
+    public class CD4 : Controller
+    {
+        ApplicationDbContext _context;
+        public CD4(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            ClientesTiposItems cti = new ClientesTiposItems();
+            cti.Clientes = _context.Tclientes.ToList();
+            cti.Tipos = _context.Ttipos.ToList();
+            cti.Items = _context.Titems.ToList();
+            return View(cti);
+        }
+
+        public IActionResult Criar() 
+        {
+            return View();
+        }
+    }
+}
---
+++ ExCliX/Models/ClientesTiposItems.cs
@@ -0,0 +1,9 @@
+﻿namespace ExCliX.Models
+{
+    public class ClientesTiposItems
+    {
+        public IEnumerable<Cliente>? Clientes { get; set; }
+        public IEnumerable<Tipo>?  Tipos { get; set; }
+        public IEnumerable<Item>?  Items { get; set; }
+    }
+}
